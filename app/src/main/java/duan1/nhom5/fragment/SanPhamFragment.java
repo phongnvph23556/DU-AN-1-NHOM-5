49134d8380d166df5d59add40048974702247776
phongnvph23556 <phongnvph23556@fpt.edu.vn>
2022-11-28 17:54:06 +0700

    xong 4 adapter

--- app/src/main/java/duan1/nhom5/fragment/SanPhamFragment.java
+++ app/src/main/java/duan1/nhom5/fragment/SanPhamFragment.java
@@ -4,18 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import duan1.nhom5.Adapter.SanPhamAdapter;
+import duan1.nhom5.DAO.SanPhamDAO;
 import duan1.nhom5.MainActivity;
 import duan1.nhom5.R;
 
 
 public class SanPhamFragment extends Fragment {
-    ImageView backsanpham;
+    private RecyclerView rcv_sanpham;
+    private ImageView backsanpham;
+    private SanPhamDAO sanPhamDAO;
+    private SanPhamAdapter sanPhamAdapter;
 
     public static SanPhamFragment newInstance() {
         SanPhamFragment fragment = new SanPhamFragment();
@@ -33,12 +40,23 @@ public class SanPhamFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v= inflater.inflate(R.layout.fragment_san_pham, container, false);
-        backsanpham=v.findViewById(R.id.backsanpham);
+        View v = inflater.inflate(R.layout.fragment_san_pham, container, false);
+        backsanpham = v.findViewById(R.id.backsanpham);
+        rcv_sanpham=v.findViewById(R.id.rcv_sanpham);
+        sanPhamDAO=new SanPhamDAO(getActivity());
+        sanPhamAdapter=new SanPhamAdapter(getActivity());
+
+        LinearLayoutManager layoutManager=new LinearLayoutManager(getActivity(),RecyclerView.VERTICAL,false);
+        rcv_sanpham.setLayoutManager(layoutManager);
+        sanPhamAdapter.setData(sanPhamDAO.selectAll());
+        rcv_sanpham.setAdapter(sanPhamAdapter);
+
+
+
         backsanpham.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getActivity(), MainActivity.class);
+                Intent intent = new Intent(getActivity(), MainActivity.class);
                 startActivity(intent);
             }
         });
