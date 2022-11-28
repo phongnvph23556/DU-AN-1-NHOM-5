1c71d1dd3e09df43ffcab20dff7c2a4060ffe3c0
phongnvph23556 <phongnvph23556@fpt.edu.vn>
2022-11-28 17:16:17 +0700

    ok

---
+++ app/src/main/java/duan1/nhom5/Adapter/SanPhamAdapter.java
@@ -0,0 +1,18 @@
+package duan1.nhom5.Adapter;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SanPhamAdapter {
+    public class UserViewHolder extends RecyclerView.ViewHolder {
+private TextView masp,maloaisp,tensp,giaban;
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            masp=itemView.findViewById(R.id.)
+        }
+    }
+
+}
